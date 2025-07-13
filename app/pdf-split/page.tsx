fbc914a2981b853364e956147c08acc9556e51d2
vin-dKR <vinodkumarmurmu62@gmail.com>
2025-07-13 11:32:10 +0530

    small fix ui

--- app/pdf-split/page.tsx
+++ app/pdf-split/page.tsx
@@ -11,16 +11,17 @@ const PDFPreviewer = dynamic(() => import('@/components/pdf-splitter/PDFPreviewe
 const Page = () => {
 
     return (
-        <div className="relative w-full text-white overflow-x-auto px-4">
-            <div className="max-w-5xl mx-auto py-4">
-                <h1 className="text-3xl font-bold text-center mb-4">PDF Splitt</h1>
-                    <PDFUploader />
+        <div className="min-h-screen mx-auto sm:p-4 w-full md:w-2xl lg:w-5xl">
+            <h1 className="text-2xl sm:text-3xl font-bold text-center mb-2 sm:mb-4">PDF Split</h1>
+            <div className="mb-4 sm:mb-6">
+                <PDFUploader />
             </div>
-            <div className="flex flex-col xl:flex-row gap-4 xl:gap-8 max-w-5xl mx-auto py-4">
-                <div className="w-full xl:w-2/3 border-white/5 rounded-lg flex justify-center">
+
+            <div className="max-w-5xl mx-auto flex flex-col lg:flex-row gap-4 lg:gap-6">
+                <div className="w-full lg:w-2/3">
                     <PDFPreviewer />
                 </div>
-                <div className="w-full xl:w-1/3 flex flex-col gap-6">
+                <div className="w-full lg:w-1/3 flex flex-col gap-6">
                     <SplitList />
                     <ExportPanel />
                 </div>
